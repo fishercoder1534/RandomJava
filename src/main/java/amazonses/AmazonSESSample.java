5370e71390d85d1e3b74673287631c1b534dd058
Steve Sun <stevesun@squareup.com>
2017-12-21 15:16:51 -0800

    add a simple AWS SES example

---
+++ src/main/java/amazonses/AmazonSESSample.java
@@ -0,0 +1,71 @@
+package amazonses;
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClient;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+import java.io.IOException;
+
+public class AmazonSESSample {
+  // Replace sender@example.com with your "From" address.
+  // This address must be verified with Amazon SES.
+  static final String FROM = "fishercoder@gmail.com";
+
+  // Replace recipient@example.com with a "To" address. If your account
+  // is still in the sandbox, this address must be verified.
+  static final String TO = "stevejsun@gmail.com";
+
+  // The configuration set to use for this email. If you do not want to use a
+  // configuration set, comment the following variable and the
+  // .withConfigurationSetName(CONFIGSET); argument below.
+  //static final String CONFIGSET = "ConfigSet";
+
+  // The subject line for the email.
+  static final String SUBJECT = "Amazon SES test (AWS SDK for Java)";
+
+  // The HTML body for the email.
+  static final String HTMLBODY = "<h1>Amazon SES test (AWS SDK for Java)</h1>"
+      + "<p>This email was sent with <a href='https://aws.amazon.com/ses/'>"
+      + "Amazon SES</a> using the <a href='https://aws.amazon.com/sdk-for-java/'>"
+      + "AWS SDK for Java</a>";
+
+  // The email body for recipients with non-HTML email clients.
+  static final String TEXTBODY = "This email was sent through Amazon SES "
+      + "using the AWS SDK for Java.";
+
+  public static void main(String[] args) throws IOException {
+
+    try {
+      BasicAWSCredentials credentials = new BasicAWSCredentials("ACCESS_KEY", "SECRET_KEY");
+      AmazonSimpleEmailService client =
+          new AmazonSimpleEmailServiceClient(credentials).withRegion(Regions.US_WEST_2);
+          //AmazonSimpleEmailServiceClientBuilder.standard().withRegion(Regions.US_WEST_2).build();
+      SendEmailRequest request = new SendEmailRequest()
+          .withDestination(
+              new Destination().withToAddresses(TO))
+          .withMessage(new Message()
+              .withBody(new Body()
+                  .withHtml(new Content()
+                      .withCharset("UTF-8").withData(HTMLBODY))
+                  .withText(new Content()
+                      .withCharset("UTF-8").withData(TEXTBODY)))
+              .withSubject(new Content()
+                  .withCharset("UTF-8").withData(SUBJECT)))
+          .withSource(FROM)
+          // Comment or remove the next line if you are not using a
+          // configuration set
+          //.withConfigurationSetName(CONFIGSET)
+      ;
+      client.sendEmail(request);
+      System.out.println("Email sent!");
+    } catch (Exception ex) {
+      System.out.println("The email was not sent. Error message: "
+          + ex.getMessage());
+    }
+  }
+}
