e3d1353dc75602f46a9db8fa173dc3bd202143d4
Fisher Coder <fishercoder@gmail.com>
2021-08-01 08:43:16 -0700

    add more comments

--- src/main/java/multithread/ThreadLocalExample.java
+++ src/main/java/multithread/ThreadLocalExample.java
@@ -5,45 +5,48 @@ package multithread;
  * be read and written by the same thread. Thus, even if two threads are
  * executing the same code, and the code has a reference to a ThreadLocal
  * variable, then the two threads cannot see each other's ThreadLocal variables.
- * 
+ * <p>
  * Since values set on a ThreadLocal object only are visible to the thread who
  * set the value, no thread can set an initial value on a ThreadLocal using
  * set() which is visible to all threads.
  * Instead you can specify an initial value for a ThreadLocal object by
  * subclassing ThreadLocal and overriding the initialValue() method. Here is how
  * that looks:
- * 
+ * <p>
  * private ThreadLocal myThreadLocal = new ThreadLocal<String>() {
- * @Override 
- * protected String initialValue() { return "This is the initial value"; } 
+ *
+ * @Override protected String initialValue() { return "This is the initial value"; }
  * };
  */
 
 public class ThreadLocalExample {
 
-	public static class MyRunnable implements Runnable {
+    public static class MyRunnable implements Runnable {
 
-		private ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
+        private ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
 
-		@Override
-		public void run() {
-			threadLocal.set((int) (Math.random() * 100D));
-			try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			System.out.println("threadLocal.get() is: " + threadLocal.get());
-		}
+        @Override
+        public void run() {
+            Integer value = (int) (Math.random() * 100D);
+            System.out.println("value = " + value);
+            threadLocal.set(value);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("threadLocal.get() is: " + threadLocal.get());
+        }
 
-		public static void main(String... args) {
-			MyRunnable sharedRunnableInstance = new MyRunnable();
-			Thread t1 = new Thread(sharedRunnableInstance);
-			Thread t2 = new Thread(sharedRunnableInstance);
-
-			t1.start();
-			t2.start();
-		}
-	}
+        public static void main(String... args) {
+            System.out.println("Program started.");
+            MyRunnable sharedRunnableInstance = new MyRunnable();
+            Thread t1 = new Thread(sharedRunnableInstance);
+            Thread t2 = new Thread(sharedRunnableInstance);
+            t1.start();
+            t2.start();
+            System.out.println("Program finished.");
+        }
+    }
 
 }
