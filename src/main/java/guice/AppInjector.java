20f7700d63b1124b896930804b4bafa5a840d48f
Fisher <fishercoder@gmail.com>
2022-07-03 14:23:12 -0700

    enable binding

--- src/main/java/guice/AppInjector.java
+++ src/main/java/guice/AppInjector.java
@@ -10,9 +10,9 @@ import com.google.inject.AbstractModule;
 public class AppInjector extends AbstractModule {
   @Override protected void configure() {
     //bind the service to implementation class
-    //bind(MessageService.class).to(EmailService.class);
+//    bind(MessageService.class).to(EmailService.class);
 
     //bind MessageService to Facebook Message implementation
-    //bind(MessageService.class).to(FacebookService.class);
+    bind(MessageService.class).to(FacebookService.class);
   }
 }
