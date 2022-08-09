c5eb4d0cd3ed50c1b87a4a8904c1deb1b2544452
Fisher <fishercoder@gmail.com>
2022-08-09 08:50:21 -0700

    add thread.join()

--- src/main/java/multithread/Worker.java
+++ src/main/java/multithread/Worker.java
@@ -10,27 +10,50 @@ import java.util.List;
 public class Worker implements Runnable {
     public boolean running = false;
 
+    @Override
+    public void run() {
+        this.running = true;
+        System.out.println("Thread " + Thread.currentThread().getName() + " is running.");
+
+        try {
+            //this pauses this spawned thread to sleep for 5 seconds.
+            System.out.println("Thread " + Thread.currentThread().getName() + " sleeps for 3 seconds");
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            //an InterruptedException should never be swallowed
+            Thread.currentThread().interrupt();
+        }
+        this.running = false;
+        System.out.println("Thread " + Thread.currentThread().getName() + " is no longer running.");
+    }
+
+
     public static void main(String... args) throws InterruptedException {
         Worker mainThreadWorker = new Worker();
         List<Thread> workers = new ArrayList<>();
 
-        System.out.println("Main thread is running with thread ID: " + Thread.currentThread().getId());
+        System.out.println("Main thread is running with thread ID: " + Thread.currentThread().getName());
 
         Date start = new Date();
 
         //let's start 10 workers
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             System.out.println("i = " + i);
             Thread thread = new Thread(mainThreadWorker);
+            thread.setName(i + "");
             workers.add(thread);
             thread.start();
+
+            //with this join() method, all 10 threads will be invoked in sequence, without it, all 10 threads will run in parallel.
+            //thread.join();
         }
+        System.out.println("There's a total of " + workers.size() + " threads.");
 
         //now we need to force the MAIN thread to wait until all Worker threads end, so we could calculate the duration
-        for (Thread worker : workers){
-            System.out.println("Checking thread " + worker.currentThread().getId() + " now.");
+        for (Thread worker : workers) {
+            System.out.println("Checking thread " + worker.currentThread().getName() + " now.");
             System.out.println("worker.isAlive() = " + worker.isAlive());
-            while (worker.isAlive()){
+            while (worker.isAlive()) {
                 Thread.sleep(1000);
             }
         }
@@ -39,21 +62,6 @@ public class Worker implements Runnable {
 
         Long duration = end.getTime() - start.getTime();
 
-        System.out.println("This whole process took " + duration/1000 + " seconds.");
-    }
-
-    @Override
-    public void run() {
-        this.running = true;
-        System.out.println("Thread " + Thread.currentThread().getId() + " is running.");
-
-        try {
-            //this pauses this spawned thread to sleep for 5 seconds.
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            //an InterruptedException should never be swallowed
-            Thread.currentThread().interrupt();
-        }
-        this.running = false;
+        System.out.println("This whole process took " + duration / 1000 + " seconds.");
     }
 }
