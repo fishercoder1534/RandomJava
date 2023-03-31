7c5c5fce85772c21006574a19b3c4ad36710e5cf
Fisher Coder <fishercoder@gmail.com>
2023-03-31 11:11:24 -0700

    remove unnecessary constructor params annotations

--- src/test/b2SdkExamples/B2JsonTest.java
+++ src/test/b2SdkExamples/B2JsonTest.java
@@ -111,17 +111,7 @@ public class B2JsonTest {
         @B2Json.optional(omitNull = true)
         public final String reason;
 
-        @B2Json.constructor(params = "" +
-                "str," +
-                "message," +
-                "aMessage," +
-                "bMessage," +
-                "pMessage," +
-                "zMessage," +
-                "localDateTime," +
-                "localDate," +
-                "reason"
-        )
+        @B2Json.constructor
         public TestRequest(Integer str, String message, String aMessage, String bMessage, String pMessage, String zMessage, LocalDateTime localDateTime, LocalDate localDate, String reason) {
             this.str = str;
             this.message = message;
