2b0a8643a7601260abc919b2e3bacdd2a25293da
Fisher Coder <fishercoder@gmail.com>
2023-04-29 17:55:08 -0700

    add LocalDateTime examples

---
+++ src/main/java/randomExamples/DateVsLocalDateTime.java
@@ -0,0 +1,23 @@
+package randomExamples;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class DateVsLocalDateTime {
+    public static void main(String... args) throws InterruptedException {
+        Date startDate = new Date();
+        LocalDateTime startLocalDateTime = LocalDateTime.now();
+        System.out.println("date is: " + startDate);
+        System.out.println("localDateTime is: " + startLocalDateTime);
+        Thread.sleep(1000);
+        Date endDate = new Date();
+        LocalDateTime endLocalDateTime = LocalDateTime.now();
+        System.out.println("date is: " + endDate);
+        System.out.println("localDateTime is: " + endLocalDateTime);
+
+        long duration1 = ((endDate.getTime() - startDate.getTime()) / 1000);
+        long duration2 = (endLocalDateTime.getSecond() - startLocalDateTime.getSecond());
+        System.out.println("duration1 is: " + duration1);
+        System.out.println("duration2 is: " + duration2);
+    }
+}
