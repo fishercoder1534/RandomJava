fbe20d0425cf0311cafaa0c81b7f36b221303b34
Fisher Coder <fishercoder@gmail.com>
2023-05-31 14:15:03 -0700

    add comment for a simple test function

--- src/main/java/profiler/Main.java
+++ src/main/java/profiler/Main.java
@@ -1,6 +1,14 @@
 package profiler;
 
 public class Main {
+    /**
+     * From this post: https://stackoverflow.com/a/9415368/4117496
+     * To run this: go to terminal and use this command:
+     * java -server -XX:+UnlockDiagnosticVMOptions '-XX:CompileCommand=print,*Main.main' src/main/java/profiler/Main.java
+     * but it produces this error:
+     * Could not load hsdis-aarch64.dylib; library not loadable; PrintAssembly is disabled
+     * I didn't dig further after this.
+     * */
     public static void main(final String[] args) {
         long x = 0;
         for (int i = 0; i < 1000000; i++) {
