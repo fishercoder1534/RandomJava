114583695318a9fbb41ce71f223e484d21448888
Fisher Coder <fishercoder@gmail.com>
2023-08-20 20:59:54 -0700

    update comment

--- src/main/java/java8tutorials/functionalInterfaces/FunctionalInterfacesDemo.java
+++ src/main/java/java8tutorials/functionalInterfaces/FunctionalInterfacesDemo.java
@@ -6,7 +6,7 @@ public class FunctionalInterfacesDemo {
     /**
      * How does lambda expressions fit into Javas type system?
      * Each lambda corresponds to a given type, specified by an interface.
-     * A so called functional interface must contain exactly one abstract method declaration.
+     * A so-called functional interface must contain exactly one abstract method declaration.
      * Each lambda expression of that type will be matched to this abstract method.
      * Since default methods are not abstract you're free to add default methods to your functional interface.
      * We can use arbitrary interfaces as lambda expressions as long as the interface only contains one abstract method.
