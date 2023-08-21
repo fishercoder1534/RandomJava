8658182c25260edd74752dfd2efa621f79c96a83
Fisher Coder <fishercoder@gmail.com>
2023-08-20 18:10:47 -0700

    a simple demo for CompletableFuture class

---
+++ src/main/java/multithread/completablefutureexamples/CompletableFutureDemo.java
@@ -0,0 +1,61 @@
+package multithread.completablefutureexamples;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static org.junit.Assert.assertEquals;
+
+public class CompletableFutureDemo {
+    private static final int MILLISECONDS_TO_FINISH_A_TASK = 1000;
+
+
+    public static void main(String... args) throws Exception {
+        System.out.println("Program started.");
+        runApp();
+        System.out.println("Program ended.");
+    }
+
+    private static void runApp() throws InterruptedException, ExecutionException {
+        Future<String> completableFuture = calculateAsync();
+        String result = completableFuture.get();
+        assertEquals("Hello", result);
+    }
+
+    private static Future<String> calculateAsync() {
+        CompletableFuture<String> completableFuture = new CompletableFuture<>();
+
+        Executors.newCachedThreadPool().submit(() -> {
+            Thread.sleep(1000);
+            System.out.println("Doing some work in the thread now..");
+            completableFuture.complete("Hello");
+            return null;
+        });
+
+        return completableFuture;
+    }
+
+    static class Worker implements Runnable {
+        private String workerName;
+
+        public Worker(String workerName) {
+            this.workerName = workerName;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + " starting worker: " + workerName);
+            doWork();
+            System.out.println(Thread.currentThread().getName() + " ended for worker: " + workerName);
+        }
+
+        private void doWork() {
+            try {
+                Thread.sleep(MILLISECONDS_TO_FINISH_A_TASK);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
