c664f91c85c130d89e9d8dcab635e7e0c167cb57
Fisher Coder <fishercoder@gmail.com>
2024-01-23 11:24:57 -0800

    add a simple program under java_playground

---
+++ src/main/java/java_playground/MainApp.java
@@ -0,0 +1,8 @@
+package java_playground;
+
+public class MainApp {
+    public static void main(String... args) {
+        System.out.println("Hello world.");
+        System.out.println("Program finished.");
+    }
+}
