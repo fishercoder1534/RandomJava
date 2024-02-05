9ad20f89422fd7631eef2118e16facdc1d9d0e00
Fisher Coder <fishercoder@gmail.com>
2024-02-04 20:40:17 -0800

    update TaskRunner

--- src/main/java/java_volatile/TaskRunner.java
+++ src/main/java/java_volatile/TaskRunner.java
@@ -3,27 +3,35 @@ package java_volatile;
 public class TaskRunner {
     /**
      * This is following the examples from https://www.baeldung.com/java-volatile
+     *
+     * Volatile keyword is to deal with Java Memory Model cache coherent challenges:
+     * To ensure that updates to variables propagate predictably to other threads, we should apply the volatile modifier to those variables.
+     * This way, we can communicate with runtime and processor to not reorder any instruction involving the volatile variable.
+     * Also, processors understand that they should immediately flush any updates to these variables so that other threads could read the shared variables most up-to-date values.
      */
     private static int number;
-    private static boolean ready;
+    private volatile static boolean ready;
 
     private static class Reader extends Thread {
         @Override
         public void run() {
-            System.out.println("ready is: " + ready);
+            System.out.println(Thread.currentThread().getName() + " thread says, ready = " + ready);
             while (!ready) {
-                System.out.println("It's yielding now..");
+                System.out.println(Thread.currentThread().getName() + " is yielding now..");
                 Thread.yield();
             }
-            System.out.println("number is: " + number);
+            System.out.println(Thread.currentThread().getName() + " thread says, number = " + number);
         }
     }
 
-    public static void main(String[] args) {
-        System.out.println("Program started.");
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + " thread started now...");
         new Reader().start();
+        System.out.println(Thread.currentThread().getName() + " thread is running now...");
         number = 42;
+        Thread.sleep(6);
+        System.out.println(Thread.currentThread().getName() + " thread finishes sleeping.");
         ready = true;
-        System.out.println("Program finished.");
+        System.out.println(Thread.currentThread().getName() + " thread finished.");
     }
 }
