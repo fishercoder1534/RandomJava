d75ef5a51eb67ed1327dff1b118c37049a60e3f9
Fisher Coder <fishercoder@gmail.com>
2024-10-20 09:51:25 -0700

    [RandomJava-101] add parallel unit tests examples

---
+++ src/test/junit5/SecondParallelUnitTest.java
@@ -0,0 +1,19 @@
+package junit5;
+
+import org.junit.jupiter.api.Test;
+
+public class SecondParallelUnitTest {
+    @Test
+    public void first() throws Exception {
+        System.out.println("SecondParallelUnitTest first() start => " + Thread.currentThread().getName());
+        Thread.sleep(500);
+        System.out.println("SecondParallelUnitTest first() end => " + Thread.currentThread().getName());
+    }
+
+    @Test
+    public void second() throws Exception {
+        System.out.println("SecondParallelUnitTest second() start => " + Thread.currentThread().getName());
+        Thread.sleep(500);
+        System.out.println("SecondParallelUnitTest second() end => " + Thread.currentThread().getName());
+    }
+}
